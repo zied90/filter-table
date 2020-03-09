5a01318f78b28afa698a2097a866574a40cff7c5
zied miladi <zied.elmiladi@gmail.com>
2020-03-09 22:36:06 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'filter-table';
+export class AppComponent  implements OnInit{
+  positionList: string[] = ['Active', 'Pending', 'Issued'];
+  displayedColumns = ['position', 'name', 'email', 'status'];
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+
+  // none value
+  filterValues = {
+    position: [],
+    name: '',
+    email: '',
+    status: ''
+  };
+
+ // @ViewChild(MatPaginator) paginator: MatPaginator;
+  // form group
+  filterForm = new FormGroup({
+    position: new FormControl(),
+    name: new FormControl(),
+    email: new FormControl(),
+    status: new FormControl(),
+  });
+
+  get position() {
+    return this.filterForm.get('position');
+  }
+
+  get name() {
+    return this.filterForm.get('name');
+  }
+
+  get email() {
+    return this.filterForm.get('email');
+  }
+
+  get status() {
+    return this.filterForm.get('status');
+  }
+
+
+  ngOnInit() {
+    this.formSubscribe();
+    this.getFormsValue();
+  }
+
+  // form subscribe
+  formSubscribe() {
+    this.position.valueChanges.subscribe(positionValue => {
+      this.filterValues['position'] = positionValue;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    });
+    this.name.valueChanges.subscribe(nameValue => {
+      this.filterValues['name'] = nameValue;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    });
+    this.email.valueChanges.subscribe(emailValue => {
+      this.filterValues['email'] = emailValue;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    });
+    this.status.valueChanges.subscribe(statusValue => {
+      this.filterValues['staus'] = statusValue;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    });
+  }
+
+  // create filter
+  getFormsValue() {
+    this.dataSource.filterPredicate = (data, filter: string): boolean => {
+      let searchString = JSON.parse(filter);
+      let isPositionAvailable = false;
+      if (searchString.position.length) {
+        for (const d of searchString.position) {
+          if (data.position.trim() === d) {
+            isPositionAvailable = true;
+          }
+        }
+      } else {
+        isPositionAvailable = true;
+      }
+      const resultValue = isPositionAvailable &&
+        data.name.toString().trim().toLowerCase().indexOf(searchString.name.toLowerCase()) !== -1 &&
+        data.email.toString().trim().toLowerCase().indexOf(searchString.email.toLowerCase()) !== -1 &&
+        data.status.toString().trim().toLowerCase().indexOf(searchString.status.toLowerCase()) !== -1;
+
+      return resultValue;
+
+    };
+    this.dataSource.filter = JSON.stringify(this.filterValues);
+  }
+
+
 }
+
+
+export interface Element {
+  name: string;
+  position: string;
+  email: string;
+  status: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 'Active', name: 'name1', email: 'mail1@mail.com', status: 'Yes'},
+  {position: 'Pending', name: 'name2', email: 'mail2@mail.com', status: 'No'},
+  {position: 'Issued', name: 'name3', email: 'mail3@mail.com', status: 'Yes'},
+  {position: 'Active', name: 'name4', email: 'mail4@mail.com', status: 'No'},
+  {position: 'Pending', name: 'name5', email: 'mail5@mail.com', status: 'Yes'},
+  {position: 'Pending', name: 'name6', email: 'mail6@mail.com', status: 'No'},
+  {position: 'Active', name: 'name7', email: 'mail7@mail.com', status: 'Yes'},
+  {position: 'Pending', name: 'name8', email: 'mail8@mail.com', status: 'No'},
+  {position: 'Issued', name: 'name9', email: 'mail9@mail.com', status: 'Yes'},
+  {position: 'Active', name: 'name10', email: 'mail10@mail.com', status: 'No'},
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatCheckboxModule,
+  MatCardModule,
+  MatInputModule,
+  MatGridListModule,
+  MatButtonModule,
+  MatDialogModule,
+  MatSortModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatDatepickerModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatInputModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSortModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
